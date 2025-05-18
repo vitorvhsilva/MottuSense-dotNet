68d6625f0c51dd92847cff9061e37b63c25651dd
vitorvhsilva <dndragonbr@gmail.com>
2025-05-17 23:39:49 -0300

    Subindo a imagem da arquitetura do projeto

--- MottuSense/Motos/obj/Debug/net8.0/Motos.AssemblyInfo.cs
+++ MottuSense/Motos/obj/Debug/net8.0/Motos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Motos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11e12badd6f5075c8571a31d427864f64519a58f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15dee58c25cfa75ae377849ffcd094a30aed6254")]
 [assembly: System.Reflection.AssemblyProductAttribute("Motos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Motos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
